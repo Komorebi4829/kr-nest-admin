919afb10e1fdb6ce7bbd3ebb666213002cc2d1ae
Kent <34937017+Komorebi4829@users.noreply.github.com>
2024-01-18 15:25:54 +0800

    update config

---
+++ apps/api/.prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "singleQuote": true,
+  "semi": false,
+  "trailingComma": 'all',
+  "printWidth": 120,
+  "proseWrap": "never",
+  "endOfLine": "auto",
+  "useTabs": false,
+  "tabWidth": 4,
+  "overrides": [
+    {
+      "files": ".prettierrc",
+      "options": {
+        "parser": "json"
+      }
+    }
+  ]
+}
