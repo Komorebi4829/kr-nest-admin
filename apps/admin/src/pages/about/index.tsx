ec966e75242ca87dc8c9b652ec2f611c2d6a3ecf
Kent <34937017+Komorebi4829@users.noreply.github.com>
2024-06-13 11:55:45 +0800

    WIP(admin): about page

--- apps/admin/src/pages/about/index.tsx
+++ apps/admin/src/pages/about/index.tsx
@@ -1,3 +1,65 @@
+import { ProCard } from '@ant-design/pro-components'
+import { Col, Row, Descriptions } from 'antd'
+
 export default function AboutPage() {
-  return <div>About Page</div>
+  const { pkg, lastBuildTime } = __APP_INFO__
+
+  const { dependencies, devDependencies, version } = pkg
+  console.log('🚀 ~ file: index.tsx:8 ~ AboutPage ~ dependencies:', dependencies)
+
+  return (
+    <Row gutter={[24, 24]}>
+      <Col span={24}>
+        <ProCard title="About" bordered>
+          <a href="https://github.com/Komorebi4829/kr-nest-admin">kr-nest-admin</a> 的前端项目是基于
+          React18、Vite、Ant Design、Ant Design Pro、TypeScript 开发，
+          内置了动态路由、权限验证、并提供了常用的功能组件，帮助你快速搭建企业级中后台产品原型。
+        </ProCard>
+      </Col>
+      <Col span={24}>
+        <ProCard title="Info" bordered>
+          <Descriptions column={{ xs: 1, sm: 1, md: 2, lg: 2, xl: 2, xxl: 2 }} bordered>
+            <Descriptions.Item label="版本">{version}</Descriptions.Item>
+            <Descriptions.Item label="最后编译时间">{lastBuildTime}</Descriptions.Item>
+            <Descriptions.Item label="GitHub">
+              <a href="https://github.com/Komorebi4829/kr-nest-admin">Github</a>
+            </Descriptions.Item>
+            <Descriptions.Item label="预览地址">
+              <a href="https://kentriversnook.com/showcase/kr-nest-admin">Preview</a>
+            </Descriptions.Item>
+          </Descriptions>
+        </ProCard>
+      </Col>
+      <Col span={24}>
+        <ProCard title="Dependencies" bordered>
+          <Descriptions
+            column={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 3, xxl: 3 }}
+            bordered
+            size="small"
+          >
+            {Object.keys(dependencies).map((key) => (
+              <Descriptions.Item key={key} label={key}>
+                {dependencies[key]}
+              </Descriptions.Item>
+            ))}
+          </Descriptions>
+        </ProCard>
+      </Col>
+      <Col span={24}>
+        <ProCard title="Dev Dependencies" bordered>
+          <Descriptions
+            column={{ xs: 1, sm: 1, md: 2, lg: 3, xl: 3, xxl: 3 }}
+            bordered
+            size="small"
+          >
+            {Object.keys(devDependencies).map((key) => (
+              <Descriptions.Item key={key} label={key}>
+                {devDependencies[key]}
+              </Descriptions.Item>
+            ))}
+          </Descriptions>
+        </ProCard>
+      </Col>
+    </Row>
+  )
 }
--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,88 +1,109 @@
 import { resolve } from 'path'
 
 import react from '@vitejs/plugin-react'
+import dayjs from 'dayjs'
 import { visualizer } from 'rollup-plugin-visualizer'
 import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite'
+
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+const pkgJson = require(resolve(__dirname, './package.json'))
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: './',
-    esbuild: {
-        // drop: ['console', 'debugger'],
+  base: './',
+  esbuild: {
+    // drop: ['console', 'debugger'],
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCaseOnly',
     },
-    css: {
-        modules: {
-            localsConvention: 'camelCaseOnly',
-        },
-        // 开css sourcemap方便找css
-        devSourcemap: true,
+    // 开css sourcemap方便找css
+    devSourcemap: true,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './', 'src'),
+      '#': resolve(__dirname, './', 'typings'),
     },
-    resolve: {
-        alias: {
-            '@': resolve(__dirname, './', 'src'),
-            '#': resolve(__dirname, './', 'typings'),
-        },
-    },
-    plugins: [
-        react(),
-        // 同步tsconfig.json的path设置alias
-        tsconfigPaths(),
-        createSvgIconsPlugin({
-            // 指定需要缓存的图标文件夹
-            iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
-            // 指定symbolId格式
-            symbolId: 'icon-[dir]-[name]',
-        }),
-        visualizer({
-            open: false,
-        }),
-        Icons({ compiler: 'jsx', jsx: 'react' }),
-    ],
+  },
+  plugins: [
+    react(),
+    // 同步tsconfig.json的path设置alias
+    tsconfigPaths(),
+    createSvgIconsPlugin({
+      // 指定需要缓存的图标文件夹
+      iconDirs: [resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId格式
+      symbolId: 'icon-[dir]-[name]',
+    }),
+    visualizer({
+      open: false,
+    }),
+    Icons({ compiler: 'jsx', jsx: 'react' }),
+  ],
 
-    server: {
-        // 自动打开浏览器
-        open: false,
-        host: true,
-        port: 2221,
-        proxy: {
-            /* manage endpoint */
-            '^/manage/api': {
-                target: 'http://127.0.0.1:2121',
-                changeOrigin: true,
-                // rewrite: (path) => path.replace(/^\/api\/manage/, '/manage/api'),
-            },
-            /* client endpoint */
-            '^/api': {
-                target: 'http://127.0.0.1:2121',
-                changeOrigin: true,
-                // rewrite: (path) => path.replace(/^\/api\/client/, '/api'),
-            },
-        },
-        cors: true,
+  server: {
+    // 自动打开浏览器
+    open: false,
+    host: true,
+    port: 2221,
+    proxy: {
+      /* manage endpoint */
+      '^/manage/api': {
+        target: 'http://127.0.0.1:2121',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api\/manage/, '/manage/api'),
+      },
+      /* client endpoint */
+      '^/api': {
+        target: 'http://127.0.0.1:2121',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api\/client/, '/api'),
+      },
     },
-    build: {
-        target: 'esnext',
-        minify: 'terser',
-        // rollupOptions: {
-        //   output: {
-        //     manualChunks(id) {
-        //       if (id.includes('node_modules')) {
-        //         // 让每个插件都打包成独立的文件
-        //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
-        //       }
-        //       return null;
-        //     },
-        //   },
-        // },
-        terserOptions: {
-            compress: {
-                // 生产环境移除console
-                drop_console: true,
-                drop_debugger: true,
-            },
-        },
+    cors: true,
+  },
+  define: createDefineData(process.cwd()),
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id) {
+    //       if (id.includes('node_modules')) {
+    //         // 让每个插件都打包成独立的文件
+    //         return id.toString().split('node_modules/')[1].split('/')[0].toString();
+    //       }
+    //       return null;
+    //     },
+    //   },
+    // },
+    terserOptions: {
+      compress: {
+        // 生产环境移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
     },
+  },
 })
+
+function createDefineData(root: string) {
+  try {
+    const { dependencies, devDependencies, name, version } = pkgJson
+
+    const __APP_INFO__ = {
+      pkg: { dependencies, devDependencies, name, version },
+      lastBuildTime: dayjs().format('YYYY-MM-DD HH:mm:ss'),
+    }
+    return {
+      __APP_INFO__: JSON.stringify(__APP_INFO__),
+    }
+  } catch (error) {
+    return {}
+  }
+}
