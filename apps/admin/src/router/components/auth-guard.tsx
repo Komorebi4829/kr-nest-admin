5d669cbb59e6200d9e14a8d44fa416527e2fc50f
Kent <34937017+Komorebi4829@users.noreply.github.com>
2024-07-24 19:28:27 +0800

    fix: get user info when enter the system every time

--- apps/admin/src/router/components/auth-guard.tsx
+++ apps/admin/src/router/components/auth-guard.tsx
@@ -1,8 +1,11 @@
+import { faker } from '@faker-js/faker'
+import { useMutation } from '@tanstack/react-query'
 import { useCallback, useEffect } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
+import userService from '@/api/user'
 import PageError from '@/pages/sys/error/PageError'
-import { useUserToken } from '@/store'
+import { useUserActions, useUserToken } from '@/store'
 
 import { useRouter } from '../hooks'
 
@@ -12,16 +15,28 @@ type Props = {
 export default function AuthGuard({ children }: Props) {
   const router = useRouter()
   const { accessToken } = useUserToken()
+  const { setUserInfo } = useUserActions()
+  const getUserInfoMutation = useMutation({ mutationFn: userService.getUserInfo })
+  const getMenuTreeMutation = useMutation({ mutationFn: userService.getMenuTree })
 
   const check = useCallback(() => {
     if (!accessToken) {
       router.replace('/login')
+      return
     }
+
+    Promise.all([getUserInfoMutation.mutateAsync(), getMenuTreeMutation.mutateAsync()]).then(
+      (arr) => {
+        const [user, menuTree] = arr
+        user.permissions = menuTree
+        setUserInfo({ ...user, avatar: faker.image.avatarLegacy() })
+      },
+    )
   }, [router, accessToken])
 
   useEffect(() => {
     check()
-  }, [check])
+  }, [])
 
   return <ErrorBoundary FallbackComponent={PageError}>{children}</ErrorBoundary>
 }
