26e78a88ac8e1a38da4f2cc2258be6c94fbdf0fd
Kent <34937017+Komorebi4829@users.noreply.github.com>
2024-08-22 20:39:25 +0800

    fix: remove vercel/analytics

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -2,7 +2,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 // react
-import { Analytics } from '@vercel/analytics/react'
 import { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 // react helmet
@@ -40,7 +39,6 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <Suspense>
-        <Analytics />
         <App />
       </Suspense>
     </QueryClientProvider>
